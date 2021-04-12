41e9f261fd3471c1d6dcdf04d5255a4ff8ecc215
chentimothy1 <timothychen1@protonmail.com>
2021-04-12 14:25:02 -0700

    added findbyid for questions and quizzes

--- services/quiz-service.js
+++ services/quiz-service.js
@@ -3,6 +3,7 @@ let quizzes = require("./quizzes.json");
 // const createQuiz = () => {}
 // const updateQuiz = () => {}
 // const deleteQuiz = () => {}
+
 const findAllQuizzes = () => quizzes;
 const findQuizById = (quizId) => quizzes.find((quiz) => quiz._id === quizId);
 
